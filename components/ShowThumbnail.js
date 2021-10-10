ed4204545c13bb5511b3f88945256fa43022838a
Rick <rickyang2910@gmail.com>
2021-10-10 20:30:41 +0800

    Orig verison of the author

--- components/ShowThumbnail.js
+++ components/ShowThumbnail.js
@@ -1,19 +1,14 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 
-function MovieThumbnail({ result }) {
+function ShowThumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   const router = useRouter();
-  const test = "test";
 
   return (
     <div
-      className="flex min-w-[250px] min-h-[170px] md:min-w-[330px] md:min-h-[210px] 
-        rounded-lg overflow-hidden shadow-xl cursor-pointer 
-        border-[3px] border-[#f9f9f9] border-opacity-10  
-        hover:border-opacity-80 hover:shadow-2xl 
-        transform hover:scale-105 transition duration-300"
-      onClick={() => router.push(`/movie/${result.id}`)}
+      className="flex min-w-[250px] min-h-[170px] md:min-w-[330px] md:min-h-[210px] rounded-lg overflow-hidden shadow-xl cursor-pointer border-[3px] border-[#f9f9f9] border-opacity-10  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300"
+      onClick={() => router.push(`/show/${result.id}`)}
     >
       <Image
         src={
@@ -29,4 +24,4 @@ function MovieThumbnail({ result }) {
   );
 }
 
-export default MovieThumbnail;
+export default ShowThumbnail;
